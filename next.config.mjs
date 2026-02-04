d957c1a589665c117ae2cb262bddb5de8ff138af
Block77778 <theblockchainsolutions@gmail.com>
2026-02-04 14:13:49 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,22 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
+  compress: true,
+  onDemandEntries: {
+    maxInactiveAge: 60 * 1000,
+    pagesBufferLength: 5,
+  },
+  experimental: {
+    isrMemoryCacheSize: 0,
   },
 }
 
